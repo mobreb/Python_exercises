a13c6e68dea6aede0e5a6e76298e53bc4acc57d1
mobreb <108151924+mobreb@users.noreply.github.com>
2022-11-06 12:13:24 +0100

    Update SQL_employees.py
    Adding docs, reformatting

--- SQL_employees.py
+++ SQL_employees.py
@@ -10,8 +10,8 @@ except:
     raise ConnectionError("could not connect to the database")
 
 
-# Add an employee to DB, confirm before commiting
-def add_employee(name, surname, salary):
+def add_employee(name: str, surname: str, salary: int) -> None:
+    """Add an employee to DB by providing name, surname and salary Confirm before committing."""
     sql = f"INSERT INTO employees(name,surname,salary) VALUES ('{name}', '{surname}',{salary})"
     c.execute(sql)
 
@@ -26,8 +26,8 @@ def add_employee(name, surname, salary):
         print("cancelled")
 
 
-# Pull records from DB
 def show():
+    """Returns all records from DB."""
     sql = "SELECT * FROM employees"
     c.execute(sql)
     data = c.fetchall()
@@ -35,8 +35,8 @@ def show():
         print(f"ID: {i[0]}, name: {i[1]}, surname: {i[2]}, salary {i[3]}")
 
 
-# Delete employee from DB by searching for employee id. Print error if employee not found.
-def delete_employee(employee_surname, employee_id):
+def delete_employee(employee_surname: str, employee_id: str) -> None:
+    """Delete employee from DB by searching for employee id. Print error if employee not found."""
     sql = f"DELETE FROM employees WHERE employeeid = '{employee_id}'"
     c.execute(sql)
 
@@ -51,8 +51,8 @@ def delete_employee(employee_surname, employee_id):
         print("cancelled")
 
 
-# Amend employee information stored in a list value.
-def modify(employee_id, value):
+def modify(employee_id: str, value: list) -> None:
+    """Amend employee information. Takes in employee_id and a list containing column and value to be modified."""
     for i in value:
         sql = (
             f"UPDATE employees SET {i[0]} = '{i[1]}' WHERE employeeid = '{employee_id}'"
@@ -70,8 +70,8 @@ def modify(employee_id, value):
         print("cancelled")
 
 
-# Search employee information by either partial or full value
-def search(searched_value):
+def search(searched_value: str) -> None:
+    """Input: partial/full name or surname as a string."""
     sql = f"SELECT * FROM employees WHERE surname LIKE '%{searched_value}%' OR name LIKE '%{searched_value}%'"
     c.execute(sql)
     data = c.fetchall()
@@ -82,8 +82,8 @@ def search(searched_value):
         raise ValueError("Data not found")
 
 
-# Ensure the employee is a member of the DB
-def check_surname(surname):
+def check_surname(surname: str) -> str:
+    """Ensure the employee is a member of the DB. Input surname, return employee id"""
     sql = f"SELECT * FROM employees WHERE surname = '{surname}'"
     c.execute(sql)
     data = c.fetchall()
@@ -93,64 +93,72 @@ def check_surname(surname):
         return f"{data[0][0]}"
 
 
-while True:
+def main():
+    while True:
 
-    menu = input(
-        "1-add employee, 2-show, 3-delete employee, 4-modify, 5-search, 6-quit: "
-    )
+        menu = input(
+            "1-add employee, 2-show, 3-delete employee, 4-modify, 5-search, 6-quit: "
+        )
 
-    if menu == "1":
-        name = input("name: ")
-        surname = input("surname: ")
-        salary = int(input("salary: "))
-        add_employee(name, surname, salary)
+        if menu == "1":
+            name = input("name: ")
+            surname = input("surname: ")
+            salary = int(input("salary: "))
+            add_employee(name, surname, salary)
 
-    elif menu == "2":
-        show()
+        elif menu == "2":
+            show()
 
-    elif menu == "3":
-        surname = input("surname: ")
-        delete_employee(employee_surname=surname, employee_id=check_surname(surname))
+        elif menu == "3":
+            surname = input("surname: ")
+            delete_employee(
+                employee_surname=surname, employee_id=check_surname(surname)
+            )
 
-    # Modify employees information, option to choose which value to modify
-    elif menu == "4":
-        value = []
-        old_surname = input("employee's surname:")
-        employee_id = check_surname(old_surname)
-        change = int(input("Value to modify? 1-all, 2-name, 3-surname, 4- salary: "))
-        if change == 1:
-            name = input("new name: ")
-            new_surname = input("new surname: ")
-            salary = int(input("new salary: "))
+        # Modify employees information, option to choose which value to modify
+        elif menu == "4":
+            value = []
+            old_surname = input("employee's surname:")
+            employee_id = check_surname(old_surname)
+            change = int(
+                input("Value to modify? 1-all, 2-name, 3-surname, 4- salary: ")
+            )
+            if change == 1:
+                name = input("new name: ")
+                new_surname = input("new surname: ")
+                salary = int(input("new salary: "))
 
-            value.append(["name", name])
-            value.append(["salary", salary])
-            value.append(["surname", new_surname])
-            modify(employee_id, value)
+                value.append(["name", name])
+                value.append(["salary", salary])
+                value.append(["surname", new_surname])
+                modify(employee_id, value)
 
-        elif change == 2:
-            name = input("new name: ")
-            value.append(["name", name])
-            modify(employee_id, value)
-        elif change == 3:
-            new_surname = input("new surname: ")
-            value.append(["surname", new_surname])
-            modify(employee_id, value)
-        elif change == 4:
-            salary = input("new salary: ")
-            value.append(["salary", salary])
-            modify(employee_id, value)
+            elif change == 2:
+                name = input("new name: ")
+                value.append(["name", name])
+                modify(employee_id, value)
+            elif change == 3:
+                new_surname = input("new surname: ")
+                value.append(["surname", new_surname])
+                modify(employee_id, value)
+            elif change == 4:
+                salary = input("new salary: ")
+                value.append(["salary", salary])
+                modify(employee_id, value)
 
-    # Search employee information by partial/full value
-    elif menu == "5":
-        searched_value = input("searched_value phrase: ")
-        search(searched_value)
+        elif menu == "5":
+            searched_value = input("searched_value phrase: ")
+            search(searched_value)
 
-    # Disconnect from DB, close program
-    elif menu == "6":
-        print("quitting")
-        conn.close()
-        break
-    # Pass an error if option out of the list is not chosen
-    else:
-        print("Unknown option")
+        # Disconnect from DB, close program
+        elif menu == "6":
+            print("quitting")
+            conn.close()
+            break
+        # Pass an error if option out of the list is not chosen
+        else:
+            print("Unknown option")
+
+
+if __name__ == "__main__":
+    main()
